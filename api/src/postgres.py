11e4c15cb0378c41367b0f0c954511be2717416d
lwieckowski <lwieckowski@outlook.com>
2023-11-26 11:12:53 +0100

    Add test with mocked database

--- api/src/postgres.py
+++ api/src/postgres.py
@@ -1,13 +1,18 @@
 import os
+from functools import cache
 
 from psycopg_pool import ConnectionPool
 
-uri = os.getenv("POSTGRES_URL", "")
-pool = ConnectionPool(uri, open=True)
+
+@cache
+def get_pool():
+    uri = os.getenv("POSTGRES_URL", "")
+    return ConnectionPool(uri, open=True)
 
 
 def get_users():
     query = "SELECT * from ecommerce.users"
+    pool = get_pool()
     with pool.connection() as conn:
         users = conn.execute(query).fetchall()
     return users
--- api/test/test_api.py
+++ api/test/test_api.py
@@ -1,8 +1,23 @@
+import pytest
 from starlette.testclient import TestClient
 
-from api import app
+import api
 
-client = TestClient(app)
+client = TestClient(api.app)
+
+
+class MockDb:
+    @staticmethod
+    def get_users():
+        return [["john776", "jdoe@hotmail.com", "John", "Doe", "h6hgd82hdy6ds"]]
+
+
+mock_db = MockDb()
+
+
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch):
+    monkeypatch.setattr(api, "db", mock_db)
 
 
 def test_index():
@@ -14,3 +29,4 @@ def test_get_users():
     with client:
         response = client.get("/users")
         assert response.status_code == 200
+        assert response.json() == mock_db.get_users()
