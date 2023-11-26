449217e0f0b2e37e9790cac1b5a76a1ac5332268
lwieckowski <lwieckowski@outlook.com>
2023-11-26 08:14:09 +0100

    Add test for endpoint that calls database

--- api/test/test_api.py
+++ api/test/test_api.py
@@ -7,3 +7,9 @@ client = TestClient(app)
 def test_index():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_get_users():
+    with client:
+        response = client.get("/users")
+        assert response.status_code == 200
